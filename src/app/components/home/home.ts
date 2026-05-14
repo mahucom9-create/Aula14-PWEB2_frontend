82255513dc4affc1383e22d33924f5bf0acabb80
mahucom9 <mahucom9@gmail.com>
2026-05-14 19:17:18 -0300

    Aula13

---
+++ src/app/components/home/home.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: false,
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+  nomeProduto: string = "Curso de Angular";
+  anuncio: string = `O ${this.nomeProduto} está em promoção`;
+  idProduto: number = 123;
+  precoProduto: number = 2.59;
+  promocao: boolean = false;
+  foto : string = "img/crud.png";
+
+  testeVariaveis(): void {
+    // variáveis de string com concatenção
+    //this.anuncio = 'O' + this.nomeProduto + 'está em promoção';
+    console.log('Nome do Produto: ', this.nomeProduto);
+    console.log('Anuncio: ', this.anuncio);
+    console.log('ID: ', this.idProduto);
+    console.log('Preço: ', this.precoProduto);
+    console.log('Promoção: ', this.promocao);
+
+    
+  }
+
+}
+
