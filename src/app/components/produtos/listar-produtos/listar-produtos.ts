cc1f30aaa2471fb8fb516ab7fe587b85d682d6f0
mahucom9 <mahucom9@gmail.com>
2026-05-21 22:15:40 -0300

    listar_e_cadastrar

--- src/app/components/produtos/listar-produtos/listar-produtos.ts
+++ src/app/components/produtos/listar-produtos/listar-produtos.ts
@@ -7,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrl: './listar-produtos.css',
 })
 export class ListarProdutos {
+  listaStrings : string[] = ["Primeiro", "Segundo", "Terceiro"];
+  listarNumeros: number[] = [15, 15.18, 100];
 
+  objetoModelo = {
+    Nome : 'wall',
+    idade : 46,
+    altura : 1.99,
+    graduado :true
+  };
+
+  listarProdutos: any[] = [
+    {nome: 'Curso de Angular', precoProduto: 35.56, validade: '2026-05-21', id: 1},
+    {nome: 'Curso de Ionic', precoProduto: 50, validade: '2026-05-21', id: 2},
+    {id: 3, nome: 'Curso de Ionic Avançado', precoProduto: 50, validade: '2026-05-21'},
+  ];
+
+  constructor()
+  {
+    for (let item of this.listaStrings){
+      console.log(item);
+    }
+
+    for (const item of this.listarNumeros){
+      console.log(item);
+
+    }
+    console.log(this.objetoModelo);
+    console.log(this.objetoModelo.Nome);
+  }
 }
